a52594c7895bf93bb081d18ad4695951b46c4f27
mohan2020coder <mohan18.welcome@gmail.com>
2024-06-17 17:19:17 +0530

    dd

--- todo/src/main.ts
+++ todo/src/main.ts
@@ -5,7 +5,6 @@
 // bootstrapApplication(AppComponent, appConfig)
 //   .catch((err) => console.error(err));
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
